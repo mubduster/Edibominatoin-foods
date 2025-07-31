4ea6805dcfacb5403f3a622d9962b532f0a00bc6
mubduster <mubassirmuneeb51@gmail.com>
2025-07-31 14:56:21 +0500

    Initial project version

---
+++ src/client/java/abed/EdibominationFoodsClient.java
@@ -0,0 +1,10 @@
+package abed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EdibominationFoodsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
